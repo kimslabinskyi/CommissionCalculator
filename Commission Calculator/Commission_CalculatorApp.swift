b50767594a5e831fa9e1d54474227781a3e2c168
Kim <kim.slabinskyi@gmail.com>
2024-12-29 12:05:11 +0100

    Initial Commit

---
+++ Commission Calculator/Commission_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Commission_CalculatorApp.swift
+//  Commission Calculator
+//
+//  Created by Kim on 29.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Commission_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
