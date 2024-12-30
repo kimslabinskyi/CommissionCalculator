d1cfb85a03c47c538af3c1bfe2edc89e0803356f
Kim <kim.slabinskyi@gmail.com>
2024-12-30 10:19:42 +0100

    Make CaclulatingCell UI

---
+++ Commission Calculator/CalculatingCell.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatingCell.swift
+//  Commission Calculator
+//
+//  Created by Kim on 29.12.2024.
+//
+
+import SwiftUI
+
+struct CalculatingCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculatingCell()
+}
---
+++ Commission Calculator/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  Commission Calculator
+//
+//  Created by Kim on 29.12.2024.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextField()
+}
